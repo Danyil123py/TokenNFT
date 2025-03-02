4dde72034e71e58a537eee576142a73e2559bafe
Danyil123py <danyilvolkhov@gmail.com>
2025-03-02 12:10:40 +0100

    Add files via upload

---
+++ menu/2js.js
@@ -0,0 +1,46 @@
+document.getElementById("purchase-form").addEventListener("submit", async function (e) {
+    e.preventDefault(); // Останавливаем перезагрузку страницы
+
+    const wallet = document.getElementById("wallet").value;
+    const nfttitle = document.getElementById("nfttitle").value;
+    const nft1title = document.getElementById("nft1title").value;
+    
+
+    const bot = "7627115459:AAEiTCndAc5YzimdZf1f5PDROEaU6LDg0a0"
+    const apiUrl = `https://api.telegram.org/bot${bot}/sendMessage`;
+
+    try{
+        const res = await fetch( "https://68cxxvs3-5000.euw.devtunnels.ms/send_telegram_message" , {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({"wallet":wallet,"nfttitle":nfttitle,"nft1title":nft1title})
+            
+            })
+       
+    } catch (erorr) {
+        console.log(1);
+    }
+
+    
+    
+
+
+    if (!wallet) {
+        alert("Введите адрес вашего кошелька!");
+        return;
+    }
+
+    // Эмуляция успешной покупки
+    document.getElementById("status").textContent = "the letter was sent successfully";
+});
+
+
+
+
+
+
+
+
+
---
+++ menu/js.js
@@ -0,0 +1,20 @@
+const but25 = document.querySelector(".but25")
+
+console.log(but25)
+
+
+
+
+but25.addEventListener("click",function(){
+    alert("You are register")
+})
+
+
+
+
+
+
+
+
+
+
